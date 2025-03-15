deedf792e51bd1a913d01c9c462335e96f943ca8
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-03-16 01:41:19 +0530

    feat(api): add Swagger, CORS, AutoMapper, and FluentValidation
    - Updated `Program.cs` to include:
      - Swagger for API documentation
      - CORS configuration allowing any origin, method, and header
      - AutoMapper and FluentValidation services
    
    - Added `Swashbuckle.AspNetCore` package reference
      in `eCommerce.API.csproj` for Swagger functionality.
    
    - Initialized `libman.json` with version and default
      provider; currently has an empty libraries array.

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -23,6 +23,24 @@ builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
 // FluentValidation
 builder.Services.AddFluentValidationAutoValidation();
 
+//Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "eCommerce.API", Version = "v1" });
+});
+
+//Add Cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -30,6 +48,16 @@ app.UseExceptionHandlingMiddleware();
 
 //Routing
 app.UseRouting();
+
+//Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "eCommerce.API v1"));
+
+//cors
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 
 //Authentication
 app.UseAuthentication();
